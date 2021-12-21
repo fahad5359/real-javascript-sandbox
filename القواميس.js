374da76fa191a127000d50763d75cc07dd7adf6a
fahad5359 <fabbgm5359@gmail.com>
2021-12-21 18:44:44 +0300

    more work

--- القواميس.js
+++ القواميس.js
@@ -4,10 +4,95 @@
 
 // //////////////////////////////////////////////////////////////////// section 1 // /////////////////////////////////////////////////////////////////
 
+let per_3 = {
+    fname: "fahad",
+    age: 23,
+    favfood: "ml5yah",
+};
+// u can target the object's contnet just like an array.
 
-let namee_2={
-    namey: 'fahad',
-    last_n: 'almarri',
-    age: 23 ,
+
+// console.log(per_3["age"]);
+// // let aae=fname
+// console.log("fname");
+
+// console.clear()
+
+// Q1) make a function inside of the object to add 100 ryals to salary
+// Q2) make a function inside of the object to change the fist name to a givin name by the user.
+// let persinfo={
+//     firname: "fahad",
+//     lasname: "almarri",
+//     age:  23,
+//     favfood: "mlokhya",
+//     job: "student",
+//     salary: 1000,
+//     onee:function(){
+//         this.salary=this.salary+100
+//         // u must retuen the value for it to work...
+//         return this.salary
+//     },
+//     chngnam:function(namm){
+//         this.firname=namm
+//         return namm
+//     }
+// };
+
+// persinfo.onee()
+
+
+
+// persinfo.chngnam("kaled")
+// persinfo.chngnam("ali")
+
+
+
+
+// let profiele = {
+//     name: "fahad",
+//     age: 23,
+//     hobbie: "prrograming drawing",
+//     food: "mlo5yah",
+//     agead: function () {
+//         this.age = this.age - 3
+//         return this.age
+//     },
+// };
+
+
+let calccc = {
+    num1: 40,
+    num2: 10,
+    num3: 14,
+    num4: 45,
+    num5: 98,
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
