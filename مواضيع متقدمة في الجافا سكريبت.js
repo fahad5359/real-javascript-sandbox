ec4622d270f056034cbc1862ae539419172b58fe
fahad5359 <fabbgm5359@gmail.com>
2022-01-07 07:51:31 +0300

    section 1

---
+++ مواضيع متقدمة في الجافا سكريبت.js
@@ -0,0 +1,41 @@
+
+// let arrrrray = [1, 2, 3, 4, 5, 6, 7]
+// function ggf(array) {
+//     for (let i = 0; i < array.length; i++) {
+//         console.log(arrrrray[i] +4);
+//     }
+// }
+
+// ggf(arrrrray)
+
+// ############################################################## section 1 ######################################## ########################################
+
+// arrow function \\ => //
+
+console.clear()
+
+
+
+/* there are two good ways we can loop through this array,
+eather we use the calssic 'for loop', or we use 'foreach'
+
+اذ نبي نسوي عمليات على الفور لوب نروح نسوي عملياتنا على
+console.log(arrrrray[i] "هنا نسوي العمليات");
+
+اتما في الفور ايتش
+الباراميتر اللي حطيناه يكفي..
+
+examples bellow
+*/
+
+let colors = ["green", "yello", "black", "white"]
+let nubeerz = [1,3,4,9,8,5,6]
+
+nubeerz.forEach( (myelm) =>{
+    console.log(myelm+5);
+    // شايف كيف نقدر نعدل عليها بسهولة..
+})
+
+
+
+
