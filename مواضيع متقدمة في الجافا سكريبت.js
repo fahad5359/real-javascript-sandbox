46ae7123a9ea92bd7bd3c6108f7f4532324a242c
fahad5359 <fabbgm5359@gmail.com>
2022-01-07 08:13:51 +0300

    more work

--- مواضيع متقدمة في الجافا سكريبت.js
+++ مواضيع متقدمة في الجافا سكريبت.js
@@ -10,10 +10,9 @@
 
 // ############################################################## section 1 ######################################## ########################################
 
-// arrow function \\ => //
 
 console.clear()
-
+// es6 //
 
 
 /* there are two good ways we can loop through this array,
@@ -28,13 +27,41 @@ console.log(arrrrray[i] "هنا نسوي العمليات");
 examples bellow
 */
 
+// arrow function \\ => //
+
 let colors = ["green", "yello", "black", "white"]
 let nubeerz = [1,3,4,9,8,5,6]
 
+
+/*.foreach 
+there are two good ways we can loop through this array,
+eather we use the calssic 'for loop', or we use 'foreach'
+
+اذ نبي نسوي عمليات على الفور لوب نروح نسوي عملياتنا على
+console.log(arrrrray[i] "هنا نسوي العمليات");
+
+اتما في الفور ايتش
+الباراميتر اللي حطيناه يكفي..
+
+examples bellow
+*/ 
 nubeerz.forEach( (myelm) =>{
     console.log(myelm+5);
     // شايف كيف نقدر نعدل عليها بسهولة..
 })
+
+
+
+
+/* .Filter
+فنكشن الفلتر ,بمعنى الكلمة يفلتر , طبعا الفلتر  طريقت كتابته لازم بكندشن او شرطز
+بمعنا ,عندنا فنكشن يحتوي دالة فلتر, وعندنا اراي, داخل الفلتر حطينا شرط انه يمشي على الاراي و اي عدد اقل من 20 لا يجيبه, يروح يعطينا بس قيمت اللي فوق العشرين
+
+it contains a for loop.
+example bellow
+*/
+
+nubeerz.filter()
 
 
 
