ac5ba63a8225a3bbca6a15f47b113059ce243904
fahad5359 <fabbgm5359@gmail.com>
2022-01-13 12:33:07 +0300

    targeting items with index

--- dom/جافا سكريبت dom.js
+++ dom/جافا سكريبت dom.js
@@ -53,3 +53,26 @@ const Famillytree = {
 // from the above examplle if we wanted to pick "aysha" we would have wrote 
 // Famillytree.father.son.grandson.grandgrandson.granddgranddgranddson.granddgranddgranddgranddson.grandotr
 console.log(Famillytree.father.son.grandson.grandgrandson.granddgranddgranddson.granddgranddgranddgranddson.grandotr);
+
+// ///////////////////////////////////////////////////////////
+
+/*
+if  u go to the browser and oopend  the console and  write "document", inside of it will appear the the whole html file.
+So "document"  by using it we can  access the html 
+*/
+
+// ///////////////////////////////////////////////////////////
+
+/* there are many ways we can target a spessific  elmint in the html file :
+for example we  can target it by id ,by  class, and quary.and many more...
+*/
+
+document.querySelector("p")
+// to select all ites with the same name we write "all"
+document.querySelectorAll("p")
+
+// ||||||| 
+
+//if we want to specfy a child we can use inddex as shown bellow
+// for example we  want to target "lolo" we write:
+document.querySelectorAll("p")[4]
