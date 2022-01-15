3bd87493f07ed0d956a9df5f2edb962020768f3f
fahad5359 <fabbgm5359@gmail.com>
2022-01-15 18:16:06 +0300

    .

--- tsting/domtst.js
+++ tsting/domtst.js
@@ -1,6 +1,8 @@
 let aaa=document.querySelectorAll("p")
-aaa= aaa[2].innerText="wlk any chnged"
+aaa[2].innerText="wlk any chnged"
 console.log(aaa[2]);
+console.log(aaa[1]);
+console.log(aaa[1]);
 
 // from the p in the html files pick and change there values.
 
