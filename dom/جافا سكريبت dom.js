9c5eb3f850d97c4aac37f8727370418ffff437e6
fahad5359 <fabbgm5359@gmail.com>
2022-01-17 14:51:39 +0300

    examples 2

--- dom/جافا سكريبت dom.js
+++ dom/جافا سكريبت dom.js
@@ -33,15 +33,15 @@ const Famillytree = {
     father: {
         son: {
             grandson: {
-                grandotr:"alia",
+                grandotr: "alia",
                 grandgrandson: {
-                    grandotr:"asma",
+                    grandotr: "asma",
                     granddgranddgranddson: {
-                        grandotr:"mohra",
+                        grandotr: "mohra",
                         granddgranddgranddgranddson: {
-                            grandotr:"aysha",
+                            grandotr: "aysha",
                             granddgranddgranddgranddgrandd: {
-                                grandotr:"kkaddiga",
+                                grandotr: "kkaddiga",
 
                             }
                         }
@@ -74,7 +74,7 @@ document.querySelectorAll("p")
 
 // ||||||| 
 
-//when we target an item like paragraph by using the quaryall ,we get all of the paragraphs. but how can we target a spicifc paragraph? we can use  index value ,as we were using in arrayes ,examplles bellow.
+//when we target an item like paragraph by using the quaryall ,we get all of the paragraphs. but how can we target a spicifc paragraph? we can use  i value ,as we were using in arrayes ,examplles bellow.
 // for example we  want to target "lolo" we write:
 document.querySelectorAll("p")
 // [p#ttst, p#p0, p#p1, p#p2, p#p3, p#wow, p#wow2, p, p, p, p, p]
@@ -83,66 +83,82 @@ document.querySelectorAll("p")
 
 
 // we can save the item in a js  varible
-const paragraphh=document.querySelectorAll("p");
+const paragraphh = document.querySelectorAll("p");
 // console.log(paragraphh);
 // from this we  can manoplate the elemnt  as  we want...
 
 // another example
-const llink=document.querySelector("#Lonk")
+const llink = document.querySelector("#Lonk")
 
 
 
 // زي ما كنا نقول فوق ان الدوم يعدل على التش تي ام ل و الس اس اس  ناخذ الينك ونغير قيمته
-llink.href="https://www.amazon.com/"
+llink.href = "https://www.amazon.com/"
 // console.log(llink);
 // yes is working perfictly we manipulated the html by using js
-let bao=document.querySelector("#p0")
-bao.innerText="changed"
+let bao = document.querySelector("#p0")
+bao.innerText = "changed"
 // console.log(bao);
 // Yes if u go back to the Html file u will see the second p is calledd "ahamd", we seccefully changed it and wrote "changed", usding the dom.
 
 
-// exampples:
+//#### exampples:
 
 
 // 1) print all p for html
 
-let pp1=document.querySelector("#don")
+let pp1 = document.querySelector("#don")
 console.log(pp1);
-let pp2=document.querySelector("#don2")
+let pp2 = document.querySelector("#don2")
 console.log(pp2);
-let pp3=document.querySelector("#don3")
+let pp3 = document.querySelector("#don3")
 console.log(pp3);
-let pp4=document.querySelector("#don4")
+let pp4 = document.querySelector("#don4")
 console.log(pp4);
-let pp5=document.querySelector("#don5")
+let pp5 = document.querySelector("#don5")
 console.log(pp5);
 
 // 2) cahnge the second to "I am Learning dom"
-pp2.innerText="I am learing the dom"
+pp2.innerText = "I am learing the dom"
 
 // 3) cange the 4th to "Txt changed"
-pp3.innerText="Txt changed"
+pp3.innerText = "Txt changed"
 
 
 // the bellow example expalins that it counts as an array so we  can ddo the same array operations to them
-let opp=document.querySelectorAll("#op")
+let opp = document.querySelectorAll("#op")
 console.log(opp);
-for (let i= 0; i < opp.length; i++) {
+for (let i = 0; i < opp.length; i++) {
     console.log(opp[i].innerText);
 }
-opp[0].innerText="afsds"
+opp[0].innerText = "afsds"
 
 
 // ///////////////////////////////////////////////////////// ///////////////// section 2 ////////////////////////////////////////////////////////////
 
 // mostly will change in css 
 // we want to manipulate the css with our js
-const fahad=document.querySelector("#lolo1")
-let sotyle=getComputedStyle(fahad)
+const fahad = document.querySelector("#lolo1")
+let sotyle = getComputedStyle(fahad)
 // sotyle.color= rgb(255, 145, 0)
-fahad.style.color="aqua"
+// fahad.style.color="black"
+fahad.style.backgroundColor = "red"
+fahad.style.fontSize = "50px"
+// As shown above we were maniipulating "fahad",and palying aroundd with its properties, by using the js to maniipulating css.
 
+//#### exampples:
+
+
+// 1) print all of the p's we have  given in exa 2
+
+// let lam=document.querySelector("#lam")
+// for (let i = 0; i < lam.length; i++) {
+//     console.log(lam.children[i]);
+// }
+console.clear()
+
+// لا تنسا تحفظ كل الملفات قبل تطبق
+let hh1=document.querySelectorAll(".hola")
 
 
 
