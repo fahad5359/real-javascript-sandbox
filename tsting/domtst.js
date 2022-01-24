9d7e3f7ee6625b95289ac352f07bd1f4f9bddc18
fahad5359 <fabbgm5359@gmail.com>
2022-01-24 19:50:19 +0300

    طريقة اختيار الكلاس

--- tsting/domtst.js
+++ tsting/domtst.js
@@ -1,8 +1,13 @@
-let aaa=document.querySelectorAll("p")
-aaa[2].innerText="wlk any chnged"
-console.log(aaa[2]);
-console.log(aaa[1]);
-console.log(aaa[1]);
-
+// let aaa=document.querySelectorAll("p")
+// aaa[2].innerText="wlk any chnged"
+// console.log(aaa[2]);
+// console.log(aaa[1]);
+// console.log(aaa[1]);
 // from the p in the html files pick and change there values.
 
+// Pick the 3ed value by using the qurlselctorall
+
+let bb
+bb=document.querySelectorAll(".oom")[2]
+
+console.log(bb);
