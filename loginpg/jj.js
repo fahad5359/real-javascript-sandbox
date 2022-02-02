a02a52309ca657ee1821cdf99a212d852cdb7ce0
fahad5359 <fabbgm5359@gmail.com>
2022-02-02 18:51:12 +0300

    practice

---
+++ loginpg/jj.js
@@ -0,0 +1,4 @@
+// ddo many users ,with name and pass inside of array [{nme:"adsd",pass:"asd"},{},{},{}]
+// for loop insidde of them and check..
+// and do check password
+let usrs=[{namee:"fahad",password:"5359"},{namee:"ali",password:"1122"}]
