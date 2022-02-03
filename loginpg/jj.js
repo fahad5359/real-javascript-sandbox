68e0f3c8e485439cd70ca845650dcb87f4a1b491
fahad5359 <fabbgm5359@gmail.com>
2022-02-03 14:02:36 +0300

    login page with show pass and hide

--- loginpg/jj.js
+++ loginpg/jj.js
@@ -1,4 +1,39 @@
 // ddo many users ,with name and pass inside of array [{nme:"adsd",pass:"asd"},{},{},{}]
 // for loop insidde of them and check..
 // and do check password
-let usrs=[{namee:"fahad",password:"5359"},{namee:"ali",password:"1122"}]
+
+// side challenge 
+let usrs = [{ namee: "fahad", password: "5359" }, { namee: "ali", password: "1122" }]
+
+// تشبيك
+let nm = document.querySelector("#name")
+
+let pass = document.querySelector("#pass")
+
+
+let btn = document.querySelector("#bbtn")
+let showbtn = document.querySelector("#show")
+
+
+showbtn.addEventListener("click", () => {
+    if (pass.type === "password") {
+        pass.setAttribute("type", "number")
+    }
+    else if (pass.type === "number") {
+        pass.setAttribute("type", "password")
+
+    }
+
+})
+
+btn.addEventListener("click", () => {
+    for (let i = 0; i < usrs.length; i++) {
+
+        if (nm.value === usrs[0].namee && pass.value == usrs[0].password) {
+            console.log("nm pass is correct");
+        } else {
+            console.log("wrong");
+        }
+    }
+
+})
