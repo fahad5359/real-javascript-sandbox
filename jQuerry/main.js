5dd703eab344de41a5c313e8a11de5dab5388ce3
fahad5359 <fabbgm5359@gmail.com>
2022-02-05 13:31:29 +0300

    work on jQuery

---
+++ jQuerry/main.js
@@ -0,0 +1,19 @@
+// jqry is a llibrary to deal with dom html css
+// when using the jQury selector it works for jQury objects ,
+
+/* best parctice is  to write the code inside 
+$(document).ready(function (){
+ // CODE HERE
+    $(selector).method()
+        I
+
+})
+*/
+
+$(document).ready(() => {
+    let ii = $("#mm")
+    let oo = $("#oo")
+    oo.css("color", "red")
+    //bellow is an example on how to change value of jQuery  
+    console.log(ii.text("hello"));
+})
