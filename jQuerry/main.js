d6a215ef5dee7afbc61d0d86db038da7248cdb2d
fahad5359 <fabbgm5359@gmail.com>
2022-02-09 17:22:52 +0300

    starting to understand jqry more and more.

--- jQuerry/main.js
+++ jQuerry/main.js
@@ -18,35 +18,65 @@ $(document).ready(function (){
 //     console.log(ii);
 // });
 
-$(document).ready(()=> {
+$(document).ready(() => {
     // blow i ppicked  the h3 "my name  ahmad" and i  selected it using all of the css selectors, and  they all worked at the same time 
-    $(".hhh3").css("color","green")
-    $("#hh3").css("color","pink")
-    $("h3").css("color","red")
+    $(".hhh3").css("color", "green")
+    $("#hh3").css("color", "pink")
+    $("h3").css("color", "red")
 
 
-//    let aaa=$(".hey").css("color","green")
-//    aaa[1].css("color","pink")
-    $("#oo").siblings().css("color","black")
+    //    let aaa=$(".hey").css("color","green")
+    //    aaa[1].css("color","pink")
+    $("#oo").siblings().css("color", "black")
 });
-// ///////////////////////////////////////excrsices  1/////////////////
+//               ///////////////////////////////////////excrsices  1/////////////////
 
+/* *****
+note:
+    attr() is used t chnage the attribute same like "get attribute" .
+        in jqury set and getattibute are the same when wrettin, but in set attribute we add the  value we want.
 
-// 3. قم بإنشاء وسم من نوع img وa كتابة في ملف HTML
+            getattribut=attr("atribut")
+            setattribut=attr("atribut","value")
+*/
+let box = $("#hh3")
+box.text("my name is not ahmmad");
+console.log(box);
 
 // با. قم بتغي ير الرابط src الموجود بالوسم img
 //  same as get elment and set elmint
-// attr() is used t chnage the attribute same like "get attribute" 
 
 
 // 5. قم بتغي ير ال alt الموجود بالوسم img
-let img=$("#iimg").attr("alt","dog")
+let img = $("#iimg").attr("alt", "dog")
 console.log(img);
 // 6. قم بتغي ير ال id الموجود بالوسم a
-let aaa=$("a").attr("id","your a")
+let aaa = $("a").attr("id", "your a")
 console.log(aaa.attr("id"));
 
 // 7. قم بتغي ير الرابط href الموجود بالوسم a
-let aa=$("a").attr("href","https://login.yahoo.com/?.src=ym&lang=en-GB&done=https%3A%2F%2Fmail.yahoo.com%2F%3Fguce_referrer%3DaHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8%26guce_referrer_sig%3DAQAAACTzPUBW94n4_JyiSz1abQdGtuEY20z0l0jPDu9k_vOUfeHpEXe0Yi4arv8-oJi2cqYk4egSTyXMVQVZS5P5yVoNy6JUlEAqP7vvH3FAl5wZE_zuCvV6_j1clJE8hVtm1V9C2K85RzusyHXN9cA_ddNDXbhXJMEA1EhPcNOxb5Um")
+let aa = $("a").attr("href", "https://login.yahoo.com/?.src=ym&lang=en-GB&done=https%3A%2F%2Fmail.yahoo.com%2F%3Fguce_referrer%3DaHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8%26guce_referrer_sig%3DAQAAACTzPUBW94n4_JyiSz1abQdGtuEY20z0l0jPDu9k_vOUfeHpEXe0Yi4arv8-oJi2cqYk4egSTyXMVQVZS5P5yVoNy6JUlEAqP7vvH3FAl5wZE_zuCvV6_j1clJE8hVtm1V9C2K85RzusyHXN9cA_ddNDXbhXJMEA1EhPcNOxb5Um")
 
 
+// ///////////////////////////////////  section 2 ///////////////////////////////////////
+
+
+// after pressing the buttne we want to change it to  a deffrent text ,size, and color
+
+// IDs
+let para = $("#para");
+let bbtn_1 = $("#btn");
+let bbtn_2 = $("#btnn");
+let bbtn_3 = $("#btnnn");
+
+// always check if buutton works first or not, log somthing to check
+bbtn_1.on("click", () => {
+    para.text("java")
+})
+bbtn_2.on("click",() => {
+    para.css("color", "red")
+})
+bbtn_3.on("click",() => {
+   
+    para.css("font-size", "50px")
+})
