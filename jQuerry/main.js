9957cba9075057822e3fa49e5dc056ca5c14d250
fahad5359 <fabbgm5359@gmail.com>
2022-02-09 12:57:34 +0300

    .

--- jQuerry/main.js
+++ jQuerry/main.js
@@ -1,4 +1,5 @@
 // jqry is a llibrary to deal with dom html css
+// why use jqry ? :jqry can replace the satandrd  js operations , is make the development proccess faster.
 // when using the jQury selector it works for jQury objects ,
 
 /* best parctice is  to write the code inside 
@@ -15,4 +16,12 @@ $(document).ready(function () {
     ii.css("color","green");
     $("#mm").css("background-color","green");
     console.log(ii);
+});
+
+$(document).ready(()=> {
+    // blow i ppicked  the h3 "my name  ahmad" and i  selected it using all of the css selectors, and  they all worked at the same time 
+    $(".hhh3").css("color","green")
+    $("#hh3").css("color","pink")
+    $("h3").css("color","red")
+    
 });
