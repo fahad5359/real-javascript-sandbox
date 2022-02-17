10cea102e9343013f02b266434a5295a0b333193
fahad5359 <fabbgm5359@gmail.com>
2022-02-17 21:37:40 +0300

    المخ ودع

---
+++ Node JS/app.js
@@ -0,0 +1,32 @@
+// النقطة تعلمني وين موقعي بالضبط
+
+const Thelocalibrary = require('./locallibrary');
+// console.log("helloo node");
+// console.log("i got executed by node");
+
+// to excute file (node app.js)
+// runtime enveroonment
+// حاليا  يتم التطبيق ليس على المتصفح بل على الباك اند
+
+// function amd(a,b){
+//     return  a+b
+// }
+// console.log(amd(10,10));
+ 
+
+
+// ////////////////////////////////////////////////////
+
+
+
+// important thing is ,NPM (Node pagckge Manger) more then 1 million librarys
+// to intialise npm we write npm init
+
+// the packge.jason is intialized after
+// json is the  main comunication sorce between servers or API's
+// we can see our librays in  the  packge.jason ,we can find mostly the things  in packge.json
+
+// what if we want to export anoother library or another file (locallibrary)?
+
+
+console.log(Thelocalibrary(10,5));
---
+++ Node JS/locallibrary.js
@@ -0,0 +1,15 @@
+// هنا عندي كم فنكشن ,ابي اعيد استخدامها في كم ملف ؟
+
+const gma=(a)=>{
+    return a+a
+}
+
+const mins=(bb,aa)=>{
+    bb-aa
+}
+const dvid=(op)=>{
+    op-op
+}
+
+// bellow we authrise a sirting functions that wew can share. 
+module.exports=gma,mins
