13364c7599e61d1eba4779fab56878d9b51826f5
fahad5359 <fabbgm5359@gmail.com>
2022-02-19 02:16:00 +0300

    installed express.js

--- Node JS/app.js
+++ Node JS/app.js
@@ -1,36 +1,5 @@
-// النقطة تعلمني وين موقعي بالضبط
-
-const dvidd = require("./locallibrary")
-const multi = require("./multi")
-// const lcallib= require("./locallibrary")
-// console.log("helloo node");
-// console.log("i got executed by node");
-
-// to excute file (node app.js)
-// runtime enveroonment
-// حاليا  يتم التطبيق ليس على المتصفح بل على الباك اند
-
-// function amd(a,b){
-//     return  a+b
-// }
-// console.log(amd(10,10));
-
-
-
-// ////////////////////////////////////////////////////
-
-
-
-// important thing is ,NPM (Node pagckge Manger) more then 1 million librarys
-// to intialise npm we write npm init
-
-// the packge.jason is intialized after
-// json is the  main comunication sorce between servers or API's
-// we can see our librays in  the  packge.jason ,we can find mostly the things  in packge.json
-
-// what if we want to export anoother library or another file (locallibrary)?
-// we  cold export  it noo problem .
-
-multi.darb(10,5)
-console.log(multi.darb(10,5));
-// console.log(dvidd(10,2));
+// ? //////////////////////////////////// section 2 //////////////////////////////////////
+//* we installed express (npm i express)
+//* u can see after installing express there is a package installed with it called "node modules", it contain evrthing from librays that it need..
+// !! بعد ما تم اضافت النود باكج او النود موديول ,هنا اذا نبي نستعملها لازم نصدرها اول .هي موجودة جاهزة لاكن نسوي اكسبورت للمكتبة اللي نبيها ثم نبدا شغلنا
+// choose witch library u want ,export it ,then start working..
---
+++ Node JS/app2.js
@@ -0,0 +1,39 @@
+// النقطة تعلمني وين موقعي بالضبط
+
+const dvidd = require("./locallibrary")
+const multi = require("./multi")
+// const lcallib= require("./locallibrary")
+// console.log("helloo node");
+// console.log("i got executed by node");
+
+// to excute file (node app.js)
+// * runtime enveroonment
+// حاليا  يتم التطبيق ليس على المتصفح بل على الباك اند
+
+// function amd(a,b){
+//     return  a+b
+// }
+// console.log(amd(10,10));
+
+
+
+//? //////////////////////////////////////////////// section1 ///////////////////////////////////////////
+
+
+
+// important thing is ,NPM (Node pagckge Manger) more then 1 million librarys
+
+//! to intialise npm we write npm init                               embortent!!!
+
+// the packge.jason is intialized after
+//* json is the  main comunication sorce between servers or API's
+//* we can see our librays in  the  packge.jason ,we can find mostly the things  in packge.json
+
+//* what if we want to export anoother library or another file (locallibrary)?
+// we  cold export  it noo problem .
+
+multi.darb(10,5)
+console.log(multi.darb(10,5));
+// console.log(dvidd(10,2));
+
+//? ////////////////////////////////////////////////////////////////// section 2 /////////////////////////////////////////////////////////////////////
