9a1fb84aba71c7f69de3c4d75bc27f7a4863aace
fahad5359 <fabbgm5359@gmail.com>
2022-02-18 20:27:12 +0300

    more prgeerss

---
+++ Node JS/mathh.js
@@ -0,0 +1,15 @@
+
+const adtion=((a,b)=>{
+    return a+b
+})
+const substract=(a,b)=>{
+    return a-b
+}
+
+// we can make a colectioon of our   functions if we put them inside  of aobject
+const colc={adtion,substract}
+
+
+
+
+module.exports=colc
