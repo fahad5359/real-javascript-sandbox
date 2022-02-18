bd9a60247f837bec9c0e9503d508e307c9f10aad
fahad5359 <fabbgm5359@gmail.com>
2022-02-19 02:54:10 +0300

    trying to understand sesrver

--- Node JS/app.js
+++ Node JS/app.js
@@ -23,14 +23,20 @@ app.listen(3000)
 
 const express = require('express')
 const app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.git("/Stronk", () => {
+    // *here we spcfy the endpoint nem
+    
 })
 
-app.listen(3000,()=>{
+
+app.listen(3000, () => {
+    // *here we spcfy the port number
     // ! here we can add a function so when ever the app or the port started working it triggers this function.
     console.log("helloh");
     console.log("i am your first server");
     console.log("ctr c to turn off");
 })
+
+// ? toogether with both the port number and the end point we can look for it in the browser after starting the server. as wrtten in the bellow formant
+    // localhost:PORT/endpoint
+    // localhost:3000/Stronk 
