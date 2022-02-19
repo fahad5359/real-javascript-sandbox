5ceeb36893495d87ee28ffaaed8b638383c0432b
fahad5359 <fabbgm5359@gmail.com>
2022-02-19 22:36:37 +0300

    more node/express sandbox

--- tsting/node_sandbox/app.js
+++ tsting/node_sandbox/app.js
@@ -2,11 +2,21 @@ const express = require('express')
 const app = express()
 // ?we export the library first,and put it insd sefuhf u a oof instatnt h
 
-app.get('/hoome', function (req, res) {
-  res.send('Hello World')
+// app.get('/hoome', function (req, res) {
+//   res.send('Hello World')
+// })
+// ?app.git is a normal function(somthing,somthing)
+// ?app.git has somthing diferant, we added a function inside of it...(somthing,somthing(){})
+app.get("/asd",(req,res)=>{
+    res.send("/hello")
+    // webpage respnse
+    
+    
 })
-
 app.listen(3000,()=>{
-    console.log("helloh meh broda ");
+    console.log("helloh user ");
+    // server response
 })
+// ?app.listen has somthing diferant, we added a function inside of it...(somthing,somthing(){})
+
 // localhost:PORT/endpoint
--- tsting/node_sandbox/appp2.js
+++ tsting/node_sandbox/appp2.js
@@ -1,9 +1,24 @@
-let pargarph = document.querySelector("#kk")
-let btn =document.querySelector("#btn")
-btn.addEventListener("click",() => {
-    console.log("oejrfiw");
+// let pargarph = document.querySelector("#kk")
+// let btn =document.querySelector("#btn")
+// btn.addEventListener("click",() => {
+//     console.log("oejrfiw");
+// })
+
+// //  ? addEventListener( "somthing" , "somthing" )
+// // $(selector).click(function () {
+// // });
+
+
+
+const express=require("express")
+const ourapp=express()
+
+ourapp.get("/home",(req,res)=>{
+    // res.send("weclome to your web page")
+    res.redirect('/tsting/node_sandbox/inde.html'+req.url)
+})
+ourapp.listen(100,()=>{
+    console.log("server is on");
+
 })
 
-//  ? addEventListener( "somthing" , "somthing" )
-// $(selector).click(function () {
-// });
