ee45535fe49bdb0d802747ec851052c50ef0e1d2
fahad5359 <fabbgm5359@gmail.com>
2022-02-25 01:06:56 +0300

    strating nodmon

--- express/app.js
+++ express/app.js
@@ -13,4 +13,4 @@ app.listen(2000, () => {
 
 // ** شي مزعج جدا كل ما عدلنا شي ,لازم نروح نطفي السيرفر و نشغله 
 // ** so we are downlowding a library called nodemon(npm i nodemon) if we want to add it to ther node packge,or(npm i -g nodmon) if we want to downlod it globally,
-// ! aftert installing nodemon,  here is the syntax(nodemon "appname or filename")
+// ! aftert installing nodemon,  here is the syntax to start the app(nodemon "appname or filename")
