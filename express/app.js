2690357a5a4f90257cc5186efd84876eb17054a6
fahad5359 <fabbgm5359@gmail.com>
2022-02-28 05:41:30 +0300

    .

--- express/app.js
+++ express/app.js
@@ -13,4 +13,4 @@ app.listen(2000, () => {
 
 // ** شي مزعج جدا كل ما عدلنا شي ,لازم نروح نطفي السيرفر و نشغله 
 // ** so we are downlowding a library called nodemon(npm i nodemon) if we want to add it to ther node packge(download it),or(npm i -g nodmon) if we want to downlod it globally,
-// ! aftert installing nodemon,  here is the syntax to start the app(nodemon "appname or filename")
+// ! after installing nodemon,  here is the syntax to start the app(nodemon "appname or filename")
