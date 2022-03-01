4c2fe65fc22c40d7dbbb4aa8897a2df2e25b4f69
fahad5359 <fabbgm5359@gmail.com>
2022-03-01 06:18:51 +0300

    .

--- express/app.js
+++ express/app.js
@@ -11,7 +11,14 @@ app.listen(2000, () => {
     console.log("SERVER ON");
 })
 
+// ?the packge.json acts as a user manual
 // ** شي مزعج جدا كل ما عدلنا شي ,لازم نروح نطفي السيرفر و نشغله 
 // ** so we are downlowding a library called nodemon(npm i nodemon) if we want to add it to ther node packge(download it),or(npm i -g nodmon) if we want to downlod it globally,
 // ! after installing nodemon,  here is the syntax to start the app(nodemon "appname or filename")
+
+
 // ** in the packg.json somthing called script, we can spicfy key words to give us a crting command
+// ** عشان نشغل الاوامر اللي بالسكربت ,اللي موجود بالباكج دوت جيسون ,نحدد نوع الكوماند
+// **then  we if we want to call it we write (npm run "the nem of our command") 
+
+ 
