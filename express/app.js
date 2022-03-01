6e65c9e36f724a67fc0e1527f7b8820a13c6dcd2
fahad5359 <fabbgm5359@gmail.com>
2022-03-01 05:41:45 +0300

    api ?

--- express/app.js
+++ express/app.js
@@ -8,9 +8,10 @@ app.get("/myapp", (req, res) => {
     res.send("hello")
 })
 app.listen(2000, () => {
-    console.log("mjnoooon ya oda");
+    console.log("SERVER ON");
 })
 
 // ** شي مزعج جدا كل ما عدلنا شي ,لازم نروح نطفي السيرفر و نشغله 
 // ** so we are downlowding a library called nodemon(npm i nodemon) if we want to add it to ther node packge(download it),or(npm i -g nodmon) if we want to downlod it globally,
 // ! after installing nodemon,  here is the syntax to start the app(nodemon "appname or filename")
+// ** in the packg.json somthing called script, we can spicfy key words to give us a crting command
--- tsting/node_sandbox/appp2.js
+++ tsting/node_sandbox/appp2.js
@@ -4,29 +4,24 @@
 //     console.log("oejrfiw");
 // })
 
-// //  ? addEventListener( "somthing" , "somthing" )
-// // $(selector).click(function () {
-// // });
 
 
 
+// creating an api and what is an api
 const express=require("express");
 const ourapp=express();
 
-// ourapp.get("/home",(req,res)=>{
-//     // res.send("weclome to your web page")
-//     
-// })
-// ourapp.listen(100,()=>{
-//     console.log("server is  on");
-
-// })
 
 
 ourapp.get("/undex",(req,res)=>{
     res.send("hello")
 })
 ourapp.listen(2000,()=>{
-    console.log("server is on");
+    console.log("Server ON");
 })
 
+// 3 things: nodemon || postman || npm run
+// we use "nodmon", to atomaticlly update server after saving
+// we create somthiing  in script  it s called api
+
+// in the packg.json somthing called script, we can spicfy key words to give us a crting command
