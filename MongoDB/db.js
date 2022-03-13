8dfb576433491ae190343641992d8c3785e71476
fahad5359 <fabbgm5359@gmail.com>
2022-03-13 15:22:52 +0300

    basics of mongodb

--- MongoDB/db.js
+++ MongoDB/db.js
@@ -1,1 +1,36 @@
 // here we istalled mongoose, mongose hellp us comunicate with mongodb better
+// npm i mongoose
+
+const mongoose = require('mongoose');
+
+// here we spicfy the url of the db
+const mongoURI = "mongodb://localhost:27017/thedbpg"
+const db = mongoose.connection
+
+// connecting the db
+mongoose.connect(mongoURI, {
+    userNewUrlParser: true, useUnifiedTopology: true
+},
+    () => {
+        console.log("the db is working");
+    });
+/*
+
+mongose.connect(mongouri {},function(){
+
+})
+
+*/
+
+// extra
+// if u want to check error
+db.on("Error", (er) => {
+    console.log(er.message + "ar7b, feh problem ببب");
+});
+db.on("connected", (err) => {
+    console.log("connected is jood");
+})
+
+
+/* ================================= sectioon 2 ========================== */
+// in secion 2 will descuss how can we add delete ,ubdate data, in the database..
