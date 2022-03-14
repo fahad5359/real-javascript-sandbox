b9cce2872762fd5c030b3d7ccb1cea1cd070bccc
fahad5359 <fabbgm5359@gmail.com>
2022-03-14 23:28:05 +0300

    .

--- MongoDB/model/user.js
+++ MongoDB/model/user.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose")
 
-// fisrt we create  a schema
+// first we create  a schema
 const userZschema= new mongoose.Schema({
     fname:{type:String, required:true},
+    favdood: String
 })
 /* mon.schema({
     fav:{}
     somting: value
 }) */
+
+// bellow we create a varuble model so blueprint,so it can help us export the schema
+const Users=mongoose.model("User",userZschema)
+
+// finally we export the module so we can test it
+module.exports=Users
